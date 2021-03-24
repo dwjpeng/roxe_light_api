dc71ccbd596bb855a97058bba876071cf64d7c4b
dwjpeng <dwjpeng@126.com>
2021-03-24 14:52:17 +0800

    update testnet info

--- setup/add_roxe_mainnet.sh
+++ setup/add_roxe_mainnet.sh
@@ -2,8 +2,8 @@
 
 perl /opt/roxe_light_api/scripts/lightapi_addnet.pl \
      --network=roxe \
-     --chainid=aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906 \
-     --descr="ROXE Mainnet" --token=ROXE --dec=4 ${LIGHTAPI_DB_OPTS}
+     --chainid=1a7c1e220c79277068a08fe57bb0461ed65ccc39950324272f63cfaeb7db10c7 \
+     --descr="ROXE Testnet" --token=ROXE --dec=4 ${LIGHTAPI_DB_OPTS}
 
 
 
